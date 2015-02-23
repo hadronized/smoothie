fc9bb52c888f02f89c8472dec8be8c93f19d86b1
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-02-23 21:47:08 +0100

    Documentation.

--- src/Data/Spline.hs
+++ src/Data/Spline.hs
@@ -7,6 +7,12 @@
 -- Stability   : experimental
 -- Portability : portable
 --
+-- A 'Spline s a' represents a curve in which 'a' is very likely to be
+-- 'Additive' (see "linear") and 's' is the sampling type.
+--
+-- The library exports two useful functions: 'spline' and 'smooth'. The former
+-- enables you to create splines while the latter enables you to sample from
+-- them using their control points.
 ----------------------------------------------------------------------------
 
 module Data.Spline (
@@ -24,14 +30,16 @@ import Data.Spline.Polynomial ( Polynomial(..), bsearchLower )
 import Data.Vector ( Vector, (!?), fromList )
 
 -- |A 'Spline' is a collection of control points with associated polynomials.
--- For /n/ control points you have to pass /n-1/ polynomials. Given two control
--- points which indices are /i/ and /i+1/, interpolation on the resulting curve
--- is performed using the polynomial of indice /i/. Thus, the latest control
--- point is ignored and can be set to whatever the user wants to, even
--- 'undefined'.
+-- Given two control points which indices are /i/ and /i+1/, interpolation on
+-- the resulting curve is performed using the polynomial of indice /i/. Thus,
+-- the latest control point is ignored and can be set to whatever the user wants
+-- to, even 'undefined' – you should use hold, though. Yeah, don’t go filthy.
 data Spline s a = Spline (Vector (CP s a)) (Vector (Polynomial s a))
 
 -- |Create a spline using a list of control points and associated polynomials.
+-- Since 'spline' sorts the list before creating the 'Spline', you don’t have to
+-- ensure the list is sorted – even though you should, setting control points
+-- with no order might be… chaotic.
 spline :: (Ord a,Ord s) => [(CP s a,Polynomial s a)] -> Spline s a
 spline = uncurry spline_ . unzip . dupLast . sortBy (comparing fst)
   where
